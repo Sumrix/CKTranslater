8625b2462254b664a438463ebc75cf9de8203078
sumrix <sumrix@gmail.com>
2021-01-13 22:26:40 +0300

    Changed folder structure

---
+++ src/Core/FileName.cs
@@ -0,0 +1,24 @@
+﻿namespace Core
+{
+    /// <summary>
+    ///     Имена файлов используемые в проекте
+    /// </summary>
+    public static class FileName
+    {
+        public const string EngLetters = @"..\..\..\..\..\data\Languages\Eng.json";
+        public const string EngToRusMap = @"..\..\..\..\..\data\EngToRusMap.json";
+        public const string EngToRusScriptLines = @"..\..\..\..\..\data\DB\EngToRusScriptLines.json";
+        public const string EngToRusSimilarities = @"..\..\..\..\..\data\LettersSimilarity\EngToRus.json";
+        public const string GamePath = @"C:\Games\Crusader Kings II";
+        public const string IgnoreValues = @"..\..\..\..\..\data\IgnoreValues.txt";
+        public const string LoadOrder = @"..\..\..\..\..\data\LoadOrder.txt";
+        public const string ModsPath = @"C:\Games\CK2_UserData\mod";
+        public const string QueryCache = @"..\..\..\..\..\data\DB\QueryCache.json";
+        public const string RusLetters = @"..\..\..\..\..\data\Languages\Rus.json";
+        public const string StringKeys = @"..\..\..\..\..\data\StringKeys.txt";
+        public const string ToTranslateWords = @"..\..\..\..\..\data\ToTranslateWords.txt";
+        public const string Translations = @"..\..\..\..\..\data\DB\Translations.json";
+        public const string TransliterationRules = @"..\..\..\..\..\data\DB\TransliterationRules.json";
+        public const string WebTranslationMisses = @"..\..\..\..\..\data\DB\WebTranslationMisses.json";
+    }
+}
