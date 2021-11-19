3b4355581369f364bcd9cb5a8e5936bf75ff6447
sumrix <sumrix@gmail.com>
2021-11-19 09:54:39 +0300

    Changed paths to relative

--- src/CKTranslator.Core/FileName.cs
+++ src/CKTranslator.Core/FileName.cs
@@ -3,19 +3,19 @@
     public static class FileName
     {
         public const string BackupPath = @"..\..\..\Data\Backups\";
-        public const string EngLetters = @"D:\Projects\CKTranslator\Data\Languages\Eng.json";
-        public const string EngToRusMap = @"D:\Projects\CKTranslator\Data\EngToRusMap.json";
-        public const string EngToRusScriptLines = @"D:\Projects\CKTranslator\Data\DB\EngToRusScriptLines.json";
-        public const string EngToRusSimilarities = @"D:\Projects\CKTranslator\Data\LettersSimilarity\EngToRus.json";
-        public const string IgnoreValues = @"D:\Projects\CKTranslator\Data\IgnoreValues.txt";
-        public const string LoadOrder = @"D:\Projects\CKTranslator\Data\LoadOrder.txt";
-        public const string LogFolder = @"D:\Projects\CKTranslator\log";
-        public const string QueryCache = @"D:\Projects\CKTranslator\Data\DB\QueryCache.json";
-        public const string RusLetters = @"D:\Projects\CKTranslator\Data\Languages\Rus.json";
-        public const string StringKeys = @"D:\Projects\CKTranslator\Data\StringKeys.txt";
-        public const string ToTranslateWords = @"D:\Projects\CKTranslator\Data\ToTranslateWords.txt";
-        public const string Translations = @"D:\Projects\CKTranslator\Data\DB\Translations.json";
-        public const string TransliterationRules = @"D:\Projects\CKTranslator\Data\DB\TransliterationRules.json";
-        public const string WebTranslationMisses = @"D:\Projects\CKTranslator\Data\DB\WebTranslationMisses.json";
+        public const string EngLetters = @"..\..\..\Data\Languages\Eng.json";
+        public const string EngToRusMap = @"..\..\..\Data\EngToRusMap.json";
+        public const string EngToRusScriptLines = @"..\..\..\Data\DB\EngToRusScriptLines.json";
+        public const string EngToRusSimilarities = @"..\..\..\Data\LettersSimilarity\EngToRus.json";
+        public const string IgnoreValues = @"..\..\..\Data\IgnoreValues.txt";
+        public const string LoadOrder = @"..\..\..\Data\LoadOrder.txt";
+        public const string LogFolder = @"..\..\..\log";
+        public const string QueryCache = @"..\..\..\Data\DB\QueryCache.json";
+        public const string RusLetters = @"..\..\..\Data\Languages\Rus.json";
+        public const string StringKeys = @"..\..\..\Data\StringKeys.txt";
+        public const string ToTranslateWords = @"..\..\..\Data\ToTranslateWords.txt";
+        public const string Translations = @"..\..\..\Data\DB\Translations.json";
+        public const string TransliterationRules = @"..\..\..\Data\DB\TransliterationRules.json";
+        public const string WebTranslationMisses = @"..\..\..\Data\DB\WebTranslationMisses.json";
     }
 }
